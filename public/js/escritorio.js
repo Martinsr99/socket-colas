360deca24b93cd9739dcc18b0a5015affdc97b5e
Martinsr99 <53481948+Martinsr99@users.noreply.github.com>
2022-10-23 19:16:55 +0200

    Allow tickets to be created and prepare design

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,42 @@
+//Referencias HTML
+const lblEscritorio = document.querySelector('h1')
+const btnAtender = document.querySelector('button')
+
+const searchParams = new URLSearchParams(window.location.search)
+
+if(!searchParams.has('escritorio')){
+    window.location = 'index.html'
+    throw new Error('El escritorio es obligatorio')
+}
+
+const escritorio = searchParams.get('escritorio')
+lblEscritorio.innerText = escritorio;
+const socket = io();
 
 
-console.log('Escritorio HTML');
+
+socket.on('connect', () => {
+    // console.log('Conectado');
+    btnAtender.disabled = false;
+
+});
+
+socket.on('disconnect', () => {
+    // console.log('Desconectado del servidor');
+
+    btnAtender.disabled = true;
+});
+
+socket.on('ultimo-ticket', (ticket) => {
+    //lblNuevoTicket.innerText = ticket;
+
+});
+
+
+btnCrear.addEventListener( 'click', () => {
+    
+    // socket.emit( 'siguiente-ticket', null, ( ticket ) => {
+    //     lblNuevoTicket.innerText = ticket;
+    // });
+
+});
--- sockets/controller.js
+++ sockets/controller.js
@@ -1,14 +1,18 @@
+const { TicketControl } = require("../models/ticket-control");
 
+const ticketControl = new TicketControl();
 
 
 const socketController = (socket) => {
 
-    socket.on('enviar-mensaje', ( payload, callback ) => {
-        
-        const id = 123456789;
-        callback( id );
+    socket.emit('ultimo-ticket', ticketControl.ultimo)
 
-        socket.broadcast.emit('enviar-mensaje', payload );
+    socket.on('siguiente-ticket', ( payload, callback ) => {
+        
+        const siguiente = ticketControl.siguiente();
+        callback(siguiente);
+
+        //TODO Notificar que hay ticket nuevo que asignar
 
     })
 
